b82d38f4b537108685804c1e850253356eb4377b
dominikjagoda <dominik.jagoda881@gmail.com>
2023-08-15 18:45:23 +0200

    📝 Added docstringsto `weather_sensor.py`

--- devices/weather_sensor.py
+++ devices/weather_sensor.py
@@ -1,11 +1,22 @@
 import random
 import datetime
 import decimal
+from typing import Dict, Any
 
 
 class WeatherSensor:
 
     def __init__(self, id: int, name: str, latitude: float, longitude: float):
+        
+        """
+        Initializes a WeatherSensor instance.
+
+        Args:
+            id (int): The unique identifier for the weather sensor.
+            name (str): The name of the weather sensor.
+            latitude (float): The latitude coordinate of the sensor's location.
+            longitude (float): The longitude coordinate of the sensor's location.
+        """
 
         self._id = id
         self.name = name
@@ -14,10 +25,18 @@ class WeatherSensor:
 
     @property
     def id(self):
+        """Sets id parameter as read only"""
         return self._id
     
-    def generate_data(self) -> dict:
+    def generate_data(self) -> Dict[str, Any]:
+        """
+        Generates simulated weather data for the sensor.
 
+        Returns:
+            Dict[str, Any]: A dictionary containing generated weather data, including
+                  device ID, name, timestamp, location, and sensor data
+                  (temperature, humidity, pressure, light intensity).
+        """
         data = {
             "device_id": self._id,
             "name": self.name,
